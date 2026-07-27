2754720f55ba0946564c43a9569ca7066f7c7849
ibeginnernoob <adheilgupta@gmail.com>
2026-07-27 23:39:16 +0530

    fix: separate package for the http server

---
+++ http_server/http.go
@@ -0,0 +1,86 @@
+package http_server
+
+// package http involves asset maintenance
+// all imp methods -> storage, replication is handled by the internal server
+// http is just an entrypoint
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/iAmAdheil/distributed-file-storage/db"
+)
+
+type InternalFileServer interface {
+	Get(key string) (io.ReadCloser, error)
+	Store(key string, r io.Reader) error
+	Delete(key string) error
+}
+
+type HTTPServerOpts struct {
+	HttpAddr string
+	Internal InternalFileServer
+	DB       *db.DB
+}
+
+type HTTPServer struct {
+	HTTPServerOpts
+
+	httpAddr string
+	db       *db.DB
+}
+
+func NewHTTPServer(httpOpts HTTPServerOpts) *HTTPServer {
+	return &HTTPServer{
+		HTTPServerOpts: httpOpts,
+
+		db:       httpOpts.DB,
+		httpAddr: httpOpts.HttpAddr,
+	}
+}
+
+func (server *HTTPServer) StartHttpServer() error {
+	go server.Listen()
+	return nil
+}
+
+func (server *HTTPServer) Listen() {
+	mux := http.NewServeMux()
+
+	// Register handlers using HandleFunc (for simple functions)
+	mux.HandleFunc("GET /health", server.healthHandler)
+	mux.HandleFunc("PUT /{bucket}/{key}", validateURLParamsMiddleware(server.storeHandler, []string{"bucket", "key"}))
+	mux.HandleFunc("GET /{bucket}/{key}", validateURLParamsMiddleware(server.getHandler, []string{"bucket", "key"}))
+	mux.HandleFunc("DELETE /{bucket}/{key}", validateURLParamsMiddleware(server.deleteHandler, []string{"bucket", "key"}))
+	mux.HandleFunc("GET /{bucket}", validateURLParamsMiddleware(server.listHandler, []string{"bucket"}))
+
+	srv := &http.Server{
+		Addr:    server.httpAddr,
+		Handler: mux, // <-- This is where you hand the receptionist (mux) over to the building (srv)
+	}
+
+	// Pass the mux to http.ListenAndServe
+	fmt.Printf("Server starting on %s...\n", server.httpAddr)
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			fmt.Printf("Server (%s) start with err: %v\n", server.httpAddr, err)
+		}
+	}()
+
+	shutdown, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT)
+	defer stop()
+	<-shutdown.Done()
+
+	fmt.Printf("Shutting down server %s...\n", server.httpAddr)
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		fmt.Printf("Server (%s) shutdown with err: %v\n", server.httpAddr, err)
+	}
+	fmt.Printf("Shutdown complete (%s).\n", server.httpAddr)
+}
